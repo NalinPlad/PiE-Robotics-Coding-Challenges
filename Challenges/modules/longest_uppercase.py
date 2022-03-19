c96f5ac87902135b75aca00c066df0b20b2ae248
NalinPlad <43052612+NalinPlad@users.noreply.github.com>
2022-03-19 12:52:27 -0700

    Began work on longest_upper case(not done)

--- Challenges/modules/longest_uppercase.py
+++ Challenges/modules/longest_uppercase.py
@@ -1,2 +1,12 @@
-def longest_uppercase(input, k): # whackalenso
-    pass
+def longest_uppercase(input, k):
+    chars = []
+    longest = 0
+    for letter in input:
+        if chars.count(letter) != 0:
+            longest += 1
+        elif len(chars) > k-1:
+            return longest
+        else:
+            chars.append(letter)
+            longest += 1
+print(longest_uppercase("hello",3))
