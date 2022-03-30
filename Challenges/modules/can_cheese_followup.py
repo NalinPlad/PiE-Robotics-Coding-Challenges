afaa3f5b0143aa1aeb5b561cb0bb7d0003e98860
Whackalenso <63932931+Whackalenso@users.noreply.github.com>
2022-03-29 20:13:20 -0700

    can_cheese_followup

--- Challenges/modules/can_cheese_followup.py
+++ Challenges/modules/can_cheese_followup.py
@@ -1,2 +1,19 @@
 def can_cheese_followup(small, small_size, big, big_size, goal) -> bool:
-    pass
+    if small_size == 0: smalls = 0
+    else: smalls = min(goal//small_size, small)
+
+    if big_size == 0: bigs = 0
+    else: bigs = min((goal-(smalls*small_size))//big_size, big)
+
+    while 1:
+        total = (smalls*small_size) + (bigs*big_size)
+        if total != goal:
+            if smalls == 0:
+                return False
+            smalls -= 1
+            if total < goal:
+                if bigs == big:
+                    return False
+                bigs += 1
+        else:
+            return True
