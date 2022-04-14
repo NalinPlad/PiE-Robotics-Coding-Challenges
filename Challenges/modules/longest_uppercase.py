e627d8bc85a154c1432aa879afe242e2fe83485b
Matthew <89431199+mattyboi231@users.noreply.github.com>
2022-04-14 16:27:36 -0700

    Update longest_uppercase.py

--- Challenges/modules/longest_uppercase.py
+++ Challenges/modules/longest_uppercase.py
@@ -1,12 +1,25 @@
-def longest_uppercase(input, k):
-    chars = []
-    longest = 0
-    for letter in input:
-        if chars.count(letter) != 0:
-            longest += 1
-        elif len(chars) > k-1:
-            return longest
-        else:
-            chars.append(letter)
-            longest += 1
-print(longest_uppercase("hello",3))
+def longest_uppercase(input,k):
+  final_out = 0
+  for i in input:
+    test_letters = i
+    for j in input:
+      if  len(test_letters) == k :
+        break
+      if test_letters.find(j) >= 0:
+        pass
+      else:
+        test_letters += j
+    max = 0
+    count = 0
+    for m in input :
+      if test_letters.find(m) == -1:
+        if count > max:
+          max = count
+        count = 0
+      else:
+        count += 1 
+    if len(input) <= k:
+      return len(input)
+    if final_out < max:
+      final_out = max
+  return final_out
