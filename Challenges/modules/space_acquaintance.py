10f286214af024847dfb2f46d52ed99344e16ef2
Matthew <89431199+mattyboi231@users.noreply.github.com>
2022-04-14 20:10:56 -0700

    Update space_acquaintance.py

--- Challenges/modules/space_acquaintance.py
+++ Challenges/modules/space_acquaintance.py
@@ -1,2 +1,45 @@
-def space_acquaintance(space_from: list, space_to: list) -> int:
-    pass
+def space_acquaintance(space_from, space_to ):
+  min_Acquaintance_sum = None
+  for i in range(len(space_from)):
+    acquaintances = []
+    Astronuat3= None
+    Astronuat2 = space_from[i]
+    Astronuat1 = space_to[i]
+    Trio_acquaintances = []
+    if Astronuat2 == Astronuat1:
+      pass
+    else:
+      for j in range(len(space_from)):
+        if j == i:
+          pass
+        elif space_to[j] == Astronuat1:
+          acquaintances.append(space_from[j])
+        elif space_from[j] == Astronuat2:
+          acquaintances.append(space_to[j])
+      print(acquaintances)
+      for k in acquaintances:
+        if Astronuat3 == k:
+          pass
+        elif acquaintances.count(k) >1:
+          Astronuat3 = k
+          for j in range(len(space_from)):
+            if Trio_acquaintances.count(j) > 0 :
+              pass
+            elif space_to[j] == Astronuat1:
+              Trio_acquaintances.append(space_to[j])
+            elif space_to[j] == Astronuat2:
+              Trio_acquaintances.append(space_to[j])
+            elif space_to[j] == Astronuat3:
+              Trio_acquaintances.append(space_to[j])
+            elif space_from[j] == Astronuat1:
+              Trio_acquaintances.append(space_to[j])
+            elif space_from[j] == Astronuat2:
+              Trio_acquaintances.append(space_to[j])
+            elif space_from[j] == Astronuat3:
+              Trio_acquaintances.append(space_to[j])
+      if min_Acquaintance_sum == None: 
+        min_Acquaintance_sum = len(Trio_acquaintances)
+      elif min_Acquaintance_sum > len(Trio_acquaintances);
+        min_Acquaintance_sum = len(Trio_acquaintances)
+              
+  return min_Acquaintance_sum
