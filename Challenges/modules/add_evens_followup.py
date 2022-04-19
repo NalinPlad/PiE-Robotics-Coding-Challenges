3b1693e7f9c932543801e7768c9338cc67b8e281
Matthew <89431199+mattyboi231@users.noreply.github.com>
2022-04-19 12:08:09 -0700

    Update add_evens_followup.py

--- Challenges/modules/add_evens_followup.py
+++ Challenges/modules/add_evens_followup.py
@@ -1,2 +1,8 @@
-def add_evens_followup(nums):
-    pass
+def add_evens2(nums2):
+	added = 0
+	numsString = str(nums2)
+	for j in range(len(numsString)):
+		if j % 2 == 0 :
+		  added += int(numsString[j])
+		
+	return added
